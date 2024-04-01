53053591bb3830bc688bd72eb17cd3d1512ca607
oovaa <omarvx211@gmail.com>
2024-04-01 19:54:36 +0200

    Update extension.js and package.json to add delay setting

--- hasanah/extension.js
+++ hasanah/extension.js
@@ -40,7 +40,7 @@ function activate(context) {
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
   activate,
