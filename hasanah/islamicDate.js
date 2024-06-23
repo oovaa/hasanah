3c955e893d8035f52336365cf07f4e1024f9f76a
oovaa <omarvx211@gmail.com>
2024-06-23 05:30:11 +0200

    added file to habdle islamic date at 23-05

---
+++ hasanah/islamicDate.js
@@ -0,0 +1,20 @@
+const get_hijri_Date = async () => {
+  let today = new Date()
+  let dd = String(today.getDate()).padStart(2, '0')
+  let mm = String(today.getMonth() + 1).padStart(2, '0') //January is 0!
+  let yyyy = today.getFullYear()
+
+  let formattedDate = dd + '-' + mm + '-' + yyyy
+
+  let response = await fetch('https://api.aladhan.com/v1/gToH/' + formattedDate)
+  let data = await response.json()
+
+  const hijri_date = data['data']['hijri']['date']
+
+  const month_en = data['data']['hijri']['month'].en
+  const month_ar = data['data']['hijri']['month'].ar
+
+  return `${hijri_date} (${month_ar} / ${month_en})`
+}
+
+export { get_hijri_Date }
