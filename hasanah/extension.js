65d0012fcc449bb0b9fc1f65cadc576d8f499114
oovaa <omarvx211@gmail.com>
2024-06-23 05:34:45 +0200

    format main file at 23-05

--- hasanah/extension.js
+++ hasanah/extension.js
@@ -1,19 +1,19 @@
-const vscode = require('vscode');
-const { printRandomHadith } = require('./hadith');
-const { getAyahText, getSpecificAyah } = require('./quraan');
+const vscode = require('vscode')
+const { printRandomHadith } = require('./hadith.js')
+const { getAyahText, getSpecificAyah } = require('./quraan.js')
 
-let timerId;
+let timerId
 
 async function getText(showHadith) {
   try {
-    let text;
+    let text
     if (showHadith) {
-      const hadith = await printRandomHadith();
+      const hadith = await printRandomHadith()
       if (hadith && hadith.arab && hadith.book) {
-        text = `${hadith.arab} 🧡 book (${hadith.book})`;
+        text = `${hadith.arab} 🧡 book (${hadith.book})`
       }
     } else {
-      const ayah = await getAyahText();
+      const ayah = await getAyahText()
       if (
         ayah &&
         ayah.text &&
@@ -21,94 +21,94 @@ async function getText(showHadith) {
         ayah.surah.name &&
         ayah.numberInSurah
       ) {
-        text = `${ayah.text} ❤️ ${ayah.surah.name} (${ayah.numberInSurah})`;
+        text = `${ayah.text} ❤️ ${ayah.surah.name} (${ayah.numberInSurah})`
       }
     }
     if (!text) {
-      throw new Error('Failed to fetch text ', showHadith);
+      throw new Error('Failed to fetch text ', showHadith)
     }
-    return text;
+    return text
   } catch (error) {
-    console.log(error);
-    return `اللهم احفظ السودان واهله ❤️ سبحان الله وبحمده`;
+    console.log(error)
+    return `اللهم احفظ السودان واهله ❤️ سبحان الله وبحمده`
   }
 }
 
 function activate(context) {
-  let config = vscode.workspace.getConfiguration('hasanah');
-  let delay = config.get('delay') * 60000; // convert from milliseconds
+  let config = vscode.workspace.getConfiguration('hasanah')
+  let delay = config.get('delay') * 60000 // convert from milliseconds
 
-  let showHadith = false;
+  let showHadith = false
 
   const showText = async () => {
-    const text = await getText(showHadith);
-    vscode.window.showInformationMessage(text);
-    showHadith = !showHadith;
-  };
+    const text = await getText(showHadith)
+    vscode.window.showInformationMessage(text)
+    showHadith = !showHadith
+  }
 
-  let timerId = setInterval(showText, delay);
+  let timerId = setInterval(showText, delay)
 
   // Listen for configuration changes
   context.subscriptions.push(
     vscode.workspace.onDidChangeConfiguration((e) => {
       if (e.affectsConfiguration('hasanah.delay')) {
-        clearInterval(timerId); // Clear the old interval
+        clearInterval(timerId) // Clear the old interval
 
-        config = vscode.workspace.getConfiguration('hasanah');
-        delay = config.get('delay') * 60000; // Get the new delay
+        config = vscode.workspace.getConfiguration('hasanah')
+        delay = config.get('delay') * 60000 // Get the new delay
 
-        timerId = setInterval(showText, delay); // Create a new interval with the new delay
+        timerId = setInterval(showText, delay) // Create a new interval with the new delay
       }
     })
-  );
+  )
 
   let disposable = vscode.commands.registerCommand(
     'hasanah.getAyah',
     async function () {
       const surah = await vscode.window.showInputBox({
         prompt: 'Enter the number of the surah'
-      });
+      })
       const ayah = await vscode.window.showInputBox({
         prompt: 'Enter the number of the ayah'
-      });
+      })
 
       if (!surah || !ayah) {
         vscode.window.showInformationMessage(
           'Invalid input. Please enter a number.'
-        );
-        return;
+        )
+        return
       }
 
       try {
-        const data = await getSpecificAyah(surah, ayah);
+        const data = await getSpecificAyah(surah, ayah)
         if (data) {
           vscode.window.showInformationMessage(
             `${data.text} 💙 ${data.surah.name} (${data.numberInSurah})`
-          );
+          )
         } else {
           vscode.window.showInformationMessage(
             'No data returned from the Quraan API.'
-          );
+          )
         }
       } catch (error) {
         vscode.window.showInformationMessage(
           `اللهم احفظ السودان واهله ❤️ سبحان الله وبحمده (invalid surah/Ayah reference or Internet problem)`
-        );
+        )
       }
     }
-  );
+  )
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
 function deactivate() {
   if (timerId) {
-    clearInterval(timerId);
-    timerId = null;
+    clearInterval(timerId)
+    timerId = null
   }
 }
 
 module.exports = {
   activate,
   deactivate
-};
+}
