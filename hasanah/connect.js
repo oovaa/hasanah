692c9e301abc3c9b82ce88a3326014c10a89fd35
oovaa <omarvx211@gmail.com>
2024-07-17 12:57:22 +0200

    testing the apis prefoemance ✅

--- hasanah/connect.js
+++ hasanah/connect.js
@@ -1,20 +1,17 @@
-import { newgetAyah } from './quraan'
+import { getAyah, getSpecificAyah } from './quraan'
 
 const { printRandomHadith } = require('./hadith')
-const { getAyahText } = require('./oldquraan')
+const { oldgetAyahText, oldgetSpecificAyah } = require('./oldquraan')
 
 // displayRandomAyah
 ;(async () => {
-  console.time()
-  const aya2 = await newgetAyah() // 'https://quranapi.pages.dev/api'
-  console.log(aya2)
-  console.timeEnd()
-  console.time()
-  const aya = await getAyahText()
-  console.log(aya)
-  console.timeEnd()
+  console.time('new get spicific ayah')
+  const aya2 = await getSpecificAyah(2, 255) // 'https://quranapi.pages.dev/api'
+  // console.log(aya2)
+  console.timeEnd('new get spicific ayah')
 
+  console.time('old get spicific ayah')
+  const aya = await oldgetSpecificAyah(2, 255)
   // console.log(aya)
-  // const hadith = await printRandomHadith()
-  //   console.log(hadith)
+  console.timeEnd('old get spicific ayah')
 })()
