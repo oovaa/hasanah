fbe707a456ca7ff2b786ec75285b6817ed56122a
oovaa <omarvx211@gmail.com>
2025-02-04 11:21:39 +0200

    fix: update getSpecificAyah function calls for consistency and improved parameter handling

--- connect.js
+++ connect.js
@@ -1,8 +1,7 @@
 import { time, timeEnd } from 'console'
-import { getAyah, getSpecificAyah } from '../oldquraan'
-
-const { GetRandomHadith } = require('./hadith')
-const { oldgetAyahText, oldgetSpecificAyah } = require('./quraan')
+import { getSpecificAyah as oldgetSpecificAyah } from './oldquraan'
+import { GetRandomHadith } from './hadith'
+import { getSpecificAyah } from './quraan'
 
 // displayRandomAyah
 async function main() {
@@ -12,12 +11,12 @@ async function main() {
     // console.log(hadith)
 
     console.time('new get spicific ayah')
-    const aya2 = await getSpecificAyah('2', '255') // 'https://quranapi.pages.dev/api'
+    const aya2 = await getSpecificAyah('2', '255' , "ar") // 'https://quranapi.pages.dev/api'
     // console.log(aya2)
     console.timeEnd('new get spicific ayah')
 
     console.time('old get spicific ayah')
-    const aya = await oldgetSpecificAyah(2, 255)
+    const aya = await oldgetSpecificAyah("2", "255")
     // console.log(aya)
     console.timeEnd('old get spicific ayah')
 }
