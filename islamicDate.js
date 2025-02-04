d9ce87aaefcee3f273fdb4b74bb393e12b1a2e0a
oovaa <omarvx211@gmail.com>
2025-02-04 10:56:11 +0200

    fix: implement caching for hijri date to improve performance

--- islamicDate.js
+++ islamicDate.js
@@ -1,3 +1,5 @@
+let hijriDateCache = {}
+
 const get_hijri_Date = async () => {
     let today = new Date()
     let dd = String(today.getDate()).padStart(2, '0')
@@ -5,6 +7,8 @@ const get_hijri_Date = async () => {
     let yyyy = today.getFullYear()
 
     let formattedDate = dd + '-' + mm + '-' + yyyy
+
+    if (hijriDateCache[formattedDate]) return hijriDateCache[formattedDate]
 
     let response = await fetch(
         'https://api.aladhan.com/v1/gToH/' + formattedDate
@@ -16,7 +20,11 @@ const get_hijri_Date = async () => {
     const month_en = data['data']['hijri']['month'].en
     const month_ar = data['data']['hijri']['month'].ar
 
-    return `${hijri_date} (${month_ar} - ${month_en}) 🩵`
+    hijriDateCache[
+        formattedDate
+    ] = `${hijri_date} (${month_ar} - ${month_en}) 🩵`
+
+    return hijriDateCache[formattedDate]
 }
 
 module.exports.get_hijri_Date = get_hijri_Date
