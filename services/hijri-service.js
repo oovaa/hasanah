9db3e1e176f8520c259c016d19ed1f16f1258646
oovaa <omarvx211@gmail.com>
2026-06-12 08:09:32 +0200

    feat: implement HijriCalendarService with Ummah API

--- services/hijri-service.js
+++ services/hijri-service.js
@@ -1,32 +1,23 @@
 const { UmmahAPI } = require('../ummah-api')
 
-class HijriService {
-  constructor(api) {
-    this.api = api || new UmmahAPI()
+class HijriCalendarService {
+  constructor() {
+    this.api = new UmmahAPI()
   }
 
-  async getDate(gregorianDate = null) {
-    const endpoint = '/hijri/date'
-    const params = gregorianDate ? { date: gregorianDate } : {}
-    return this.api.get(endpoint, params)
+  async getTodayHijriDate() {
+    const data = await this.api.get('/today-hijri')
+    return this.formatHijriDate(data)
   }
 
-  async getCalendar(year, month) {
-    const endpoint = `/hijri/calendar/${year}/${month}`
-    return this.api.get(endpoint, {})
-  }
-
-  async getIslamicEvents() {
-    const endpoint = '/hijri/events'
-    return this.api.get(endpoint, {})
-  }
-
-  async getPrayerTimes(lat, lng, date = null) {
-    const endpoint = '/hijri/prayer-times'
-    const params = { lat, lng }
-    if (date) params.date = date
-    return this.api.get(endpoint, params)
+  formatHijriDate(data) {
+    return {
+      date: data.date,
+      month: data.month,
+      year: data.year,
+      day: data.day
+    }
   }
 }
 
-module.exports = { HijriService }
+module.exports = { HijriCalendarService }
